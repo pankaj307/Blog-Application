f8dab1b31e7666ba4357f7e2a33acf17a9c971d0
pankaj.kushwah <pankaj.kushwah@teladochealth.com>
2024-08-04 17:58:05 +0530

    Added CustomUserDetailService class

---
+++ blog-app-apis/src/main/java/com/maverick/blog/security/CustomUserDetailService.java
@@ -0,0 +1,26 @@
+package com.maverick.blog.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.maverick.blog.entities.User;
+import com.maverick.blog.exceptions.ResourceNotFoundException;
+import com.maverick.blog.repositories.UserRepo;
+
+public class CustomUserDetailService implements UserDetailsService{
+	
+	@Autowired
+	private UserRepo userRepo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		// load user form DB by user-name
+		User user = this.userRepo.findByEmail(username).orElseThrow(() -> new ResourceNotFoundException("User", "Email", username));
+		
+		return null;
+	}
+
+}
